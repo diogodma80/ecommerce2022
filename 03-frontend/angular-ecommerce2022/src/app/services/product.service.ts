e1a462edb7f92e1e29fcb97b2c1ecf60bda26090
diogodma80 <diogodma80@gmail.com>
2022-07-31 13:35:10 -0400

    subscribe data from Angular service and display on index.html

--- 03-frontend/angular-ecommerce2022/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce2022/src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList: Observable<Product[]> {
+  getProductList(): Observable<Product[]> {
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
