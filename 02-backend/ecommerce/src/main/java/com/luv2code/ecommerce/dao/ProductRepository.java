0ddd3e162e22ee32626f12e4c6aeb1182754491d
diogodma80 <diogodma80@gmail.com>
2022-08-05 08:51:57 -0400

    modify Spring Boot app to include search methos | API endpoint http://localhost:8080/api/products/search/findByNameContaining?name=[NAME]

--- 02-backend/ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -11,4 +11,6 @@ import com.luv2code.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
